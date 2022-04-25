d6a2d48ca44ca3607ca1c2b1b2dd64e891562f43
qirtudgus <qirtudgus@naver.com>
2022-04-26 03:44:08 +0900

    중간 배포 local -> sungtt.com

--- server/db_config.js
+++ server/db_config.js
@@ -1,11 +1,11 @@
-const mysql = require('mysql');
+const mysql = require("mysql");
 const db = mysql.createConnection({
   host: process.env.local_DB_host,
   port: process.env.local_DB_port,
   user: process.env.local_DB_user,
   password: process.env.local_DB_password,
-  database: 'saemoi',
-  charset: 'utf8mb4',
+  database: "saemoi",
+  charset: "utf8mb4",
 });
 
 module.exports = db;
