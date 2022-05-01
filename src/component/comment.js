7335fd39e1ebdbea4cf0da3437441cd0b677e243
qirtudgus <qirtudgus@naver.com>
2022-05-01 11:16:48 +0900

    comment.js에서 현재시간코드를 addDate함수로 변경

--- src/component/comment.js
+++ src/component/comment.js
@@ -3,6 +3,7 @@ import sha256 from 'crypto-js/sha256';
 import Footer from './footer';
 // import cryptoJs from "crypto-js";
 import React, { useEffect, useState, useRef } from 'react';
+import { addDate } from './addDate';
 
 const Comment = ({ getCommentApi, postCommentApi, brandName }) => {
   const [comment, setComment] = useState([]);
@@ -19,28 +20,11 @@ const Comment = ({ getCommentApi, postCommentApi, brandName }) => {
   const commentList = useRef();
 
   const postComment = async () => {
-    let hours = new Date().getHours();
-    hours = hours >= 10 ? hours : '0' + hours;
-
-    let minute = new Date().getMinutes();
-    minute = minute >= 10 ? minute : '0' + minute;
-
-    let time = hours + ':' + minute;
-
-    let day = new Date().getDate();
-    day = day >= 10 ? day : '0' + day;
-
-    let month = new Date().getMonth() + 1;
-    month = month >= 10 ? month : '0' + month;
-
-    let today = month + '.' + day;
-    let submitDate = today + ' ' + time;
-
     const value = commentContent.current.value;
     const password = passwordValue.current.value;
     const hashPassword = sha256(password).words.join('');
     const commentData = {
-      submitDate,
+      sumbitDate: addDate(),
       value,
       hashPassword,
     };
