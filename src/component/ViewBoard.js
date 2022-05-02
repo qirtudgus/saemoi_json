80f7ae5a5d085ca26ea5fbc9eff766c4f87c05ee
qirtudgus <qirtudgus@naver.com>
2022-05-03 01:32:55 +0900

    ViewBoard.js 렌더링 최적화, 게시판 ui 변경

--- src/component/ViewBoard.js
+++ src/component/ViewBoard.js
@@ -1,14 +1,14 @@
-import axios from 'axios';
-import React, { useState, useEffect, useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
-import '../css/viewboard.css';
-import backHistory from '../img/뒤로가기_흰색.svg';
-import { addDate } from './addDate';
+import axios from "axios";
+import React, { useState, useEffect, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import "../css/viewboard.css";
+import backHistory from "../img/뒤로가기_흰색.svg";
+import { addDate } from "./addDate";
 // TOAST UI Editor import
-import '@toast-ui/editor/dist/toastui-editor.css';
-import { Viewer } from '@toast-ui/react-editor';
-import { UserInfo } from '../App';
-import CommentFooter from './CommentFooter';
+import "@toast-ui/editor/dist/toastui-editor.css";
+import { Viewer } from "@toast-ui/react-editor";
+import { UserInfo } from "../App";
+import CommentFooter from "./CommentFooter";
 
 const ViewBoard = () => {
   const { userAuth, goBoard, URL, pathname } = useContext(UserInfo);
@@ -19,9 +19,15 @@ const ViewBoard = () => {
   const [commentList, setCommentList] = useState([]);
   const [checkLikeUser, setCheckLikeUser] = useState(false);
   const [checkWriteUser, setCheckWriteUser] = useState(false);
+  const key = pathname.replace(/[^0-9]/g, "");
 
-  const key = pathname.replace(/[^0-9]/g, '');
-  console.log('안녕');
+  const [arr, setArr] = useState({
+    page: [],
+    commentList: [],
+    checkWriteUser: false,
+  });
+
+  console.log("안녕");
 
   const goBack = () => {
     navigate(-1);
@@ -30,34 +36,61 @@ const ViewBoard = () => {
   //토큰의 아이디와 게시판 작성자를 비교하여 게시물 수정 삭제 버튼 생성
   function checkWriter(userAuth, writer) {
     if (userAuth === writer) {
-      setCheckWriteUser(true);
-      console.log('글의 작성자입니다.');
-      return;
+      console.log("글의 작성자입니다.");
+      return true;
     } else {
-      setCheckWriteUser(false);
-      console.log('작성자가 아닙니다.');
-      return;
+      console.log("작성자가 아닙니다.");
+      return false;
     }
   }
 
-  //게시판 api 뿌려주기
-  useEffect(() => {
-    //게시물
-    axios
+  function viewBoardData() {
+    return axios
       .post(`${URL}/api/boardApiData/viewBoard`, { key: key })
       .then((res) => {
-        // console.log(res.data);
-        setPage([...res.data]);
-        checkWriter(userAuth.id, res.data[0].board_writer);
+        return res.data;
       });
-    //댓글목록
-    axios
+  }
+
+  function viewBoardCommentData() {
+    return axios
       .post(`${URL}/api/boardApiData/comment`, { index: key })
       .then((res) => {
-        console.log(res.data);
-        setCommentList([...res.data]);
+        return res.data;
       });
+  }
+
+  useEffect(() => {
+    axios.all([viewBoardData(), viewBoardCommentData()]).then(
+      axios.spread(function (pageData, commentData) {
+        setArr({
+          ...arr,
+          page: [...pageData],
+          commentList: [...commentData],
+          checkWriteUser: checkWriter(userAuth.id, pageData[0].board_writer),
+        });
+      }),
+    );
   }, []);
+
+  //게시판 api 뿌려주기
+  // useEffect(() => {
+  //   //게시물
+  //   axios
+  //     .post(`${URL}/api/boardApiData/viewBoard`, { key: key })
+  //     .then((res) => {
+  //       // console.log(res.data);
+  //       setPage([...res.data]);
+  //       checkWriter(userAuth.id, res.data[0].board_writer);
+  //     });
+  //   //댓글목록
+  //   axios
+  //     .post(`${URL}/api/boardApiData/comment`, { index: key })
+  //     .then((res) => {
+  //       console.log(res.data);
+  //       setCommentList([...res.data]);
+  //     });
+  // }, []);
 
   // 추천 유무 확인하기
   // useEffect(() => {
@@ -78,12 +111,12 @@ const ViewBoard = () => {
       })
       .then((res) => {
         if (res.data === true) {
-          console.log('추천되었습니다.');
+          console.log("추천되었습니다.");
           return;
         } else if (res.data === false) {
           //추천이 취소되는 기능도 만들어보자..
           //db에서 -1 해주고 리스트에서 아이디를 제거해주면 될거같다.
-          console.log('추천취소!');
+          console.log("추천취소!");
         }
       })
       .then((res) => {
@@ -93,7 +126,7 @@ const ViewBoard = () => {
           })
           .then((res) => {
             // console.log(res.data);
-            setPage([...res.data]);
+            setArr({ ...arr, page: [...res.data] });
           });
       });
   };
@@ -108,18 +141,18 @@ const ViewBoard = () => {
         content: comment,
       })
       .then((res) => {
-        setComment('');
+        setComment("");
         axios
           .post(`${URL}/api/boardApiData/comment`, { index: key })
           .then((res) => {
             console.log(res.data);
-            setCommentList([...res.data]);
+            setArr({ ...arr, commentList: [...res.data] });
           });
       });
   };
 
   const removeBoard = () => {
-    if (window.confirm('글을 삭제하시겠습니까?')) {
+    if (window.confirm("글을 삭제하시겠습니까?")) {
       console.log(key);
       axios.post(`${URL}/api/boardApiData/removeBoard`, {
         key: key,
@@ -130,51 +163,59 @@ const ViewBoard = () => {
 
   return (
     <>
-      {page.reverse().map((i, index) => (
-        <div key={index} className='viewboard_warp'>
-          <div className='viewboard_container'>
-            {checkWriteUser ? (
-              <>
-                <button
-                  onClick={() => {
-                    navigate(`/board/UpdateWrite/${i.board_index}`);
-                  }}
-                >
-                  글수정
-                </button>
-                <button onClick={removeBoard}>글삭제</button>
-              </>
-            ) : null}
-
-            <p className='viewboard_title'>{i.board_title}</p>
-            <div className='viewboard_middiv'>
+      {arr.page.reverse().map((i, index) => (
+        <div key={index} className="viewboard_warp">
+          <div className="viewboard_container">
+            <div className="viewboard_middiv">
               <p>작성자 : {i.board_writer}</p>
 
-              <p>작성날짜 : {i.board_date}</p>
+              <p>{i.board_date}</p>
             </div>
-            <div className='viewboard_view'>
+            <p className="viewboard_title">{i.board_title}</p>
+
+            <div className="viewboard_view">
               <Viewer initialValue={i.board_content}></Viewer>
             </div>
-            <span>추천수 : {i.board_like}</span>
+            <p className="viewboard_like">
+              <div>
+                추천수 : {i.board_like}{" "}
+                <button
+                  className="like"
+                  onClick={() => {
+                    like(i.board_index, userAuth.id);
+                  }}
+                >
+                  추천
+                </button>
+              </div>
+              <div>
+                {arr.checkWriteUser ? (
+                  <>
+                    <button
+                      onClick={() => {
+                        navigate(`/board/UpdateWrite/${i.board_index}`);
+                      }}
+                    >
+                      글수정
+                    </button>
+                    <button onClick={removeBoard}>글삭제</button>
+                  </>
+                ) : null}
+              </div>
+            </p>
 
-            <button
-              className='like'
-              onClick={() => {
-                like(i.board_index, userAuth.id);
-              }}
-            >
-              추천
-            </button>
+            {arr.commentList.map((i, index) => (
+              <div className="comment_wrap" key={i.comment_index}>
+                <div className="comment_middiv">
+                  <p className="comment_writer">{i.comment_writer}</p>
+                  <p className="comment_date">{i.comment_date}</p>
+                </div>
 
-            {commentList.map((i, index) => (
-              <div key={i.comment_index}>
-                <p>{i.comment_date}</p>
-                <p>{i.comment_content}</p>
-                <p>{i.comment_writer}</p>
+                <p className="comment_content">{i.comment_content}</p>
               </div>
             ))}
 
-            <div className='viewboard_bottomdiv'></div>
+            <div className="viewboard_bottomdiv"></div>
             <CommentFooter
               setCommentList={setCommentList}
               key={key}
@@ -183,8 +224,8 @@ const ViewBoard = () => {
               comment={comment}
             />
           </div>
-          <div className='goBack' onClick={goBack}>
-            <img src={backHistory} alt='뒤로가기' />
+          <div className="goBack" onClick={goBack}>
+            <img src={backHistory} alt="뒤로가기" />
           </div>
         </div>
       ))}
