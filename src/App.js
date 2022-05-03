8a81235f7b658c76e87084914817151b04019193
qirtudgus <qirtudgus@naver.com>
2022-05-03 10:35:10 +0900

    buffer, util, react-s3 설치

--- src/App.js
+++ src/App.js
@@ -1,70 +1,71 @@
-import "./App.css";
-import { Route, Routes, useNavigate, useLocation } from "react-router-dom";
+import './App.css';
+import { Route, Routes, useNavigate, useLocation } from 'react-router-dom';
 
-import Oliveyoung from "./component/oliveyoung";
-import Home from "./component/home";
-import Header from "./component/header";
-import Mustit from "./component/mustit";
-import About from "./component/about";
-import NotReady from "./component/notReady";
-import Aland from "./component/aland";
-import Footerbar from "./component/Footerbar";
-import Category from "./component/Category";
-import Layout from "./component/Layout";
-import Login from "./component/auth/login";
-import Register from "./component/auth/register";
-import React, { useEffect, useState } from "react";
-import jwtDecode from "jwt-decode";
-import axios from "axios";
-import MyPage from "./component/MyPage";
-import PasswordChange from "./component/auth/PasswordChange";
-import Write from "./component/Write";
-import UpdateWrite from "./component/UpdateWrite";
-import Board from "./component/Board";
-import ViewBoard from "./component/ViewBoard";
-import CommnetFooter from "./component/CommentFooter";
+import Oliveyoung from './component/oliveyoung';
+import Home from './component/home';
+import Header from './component/header';
+import Mustit from './component/mustit';
+import About from './component/about';
+import NotReady from './component/notReady';
+import Aland from './component/aland';
+import Footerbar from './component/Footerbar';
+import Category from './component/Category';
+import Layout from './component/Layout';
+import Login from './component/auth/login';
+import Register from './component/auth/register';
+import React, { useEffect, useState } from 'react';
+import jwtDecode from 'jwt-decode';
+import axios from 'axios';
+import MyPage from './component/MyPage';
+import PasswordChange from './component/auth/PasswordChange';
+import Write from './component/Write';
+import UpdateWrite from './component/UpdateWrite';
+import Board from './component/Board';
+import ViewBoard from './component/ViewBoard';
+import CommnetFooter from './component/CommentFooter';
 
 export const UserInfo = React.createContext();
+window.Buffer = window.Buffer || require('buffer').Buffer;
 
 // api통신 시 URL 변경용
-const local = "http://localhost:3001";
-const server = "https://sungtt.com";
+const local = 'http://localhost:3001';
+const server = 'https://sungtt.com';
 // URL 할당에 따른 서버환경 변경! 아주편리하다~
 const URL = local;
 
 let vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty("--vh", `${vh}px`);
+document.documentElement.style.setProperty('--vh', `${vh}px`);
 
-window.addEventListener("resize", () => {
-  console.log("resize");
+window.addEventListener('resize', () => {
+  console.log('resize');
   let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty("--vh", `${vh}px`);
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
 });
 
 function App() {
   const href = useLocation();
   const navigate = useNavigate();
   const [userAuth, setUserAuth] = useState({
-    id: "",
+    id: '',
     auth: false,
-    refreshToken: "",
+    refreshToken: '',
   });
   const goLogOut = () => {
-    localStorage.removeItem("token");
-    setUserAuth({ ...userAuth, id: "", auth: false, refreshToken: "" });
+    localStorage.removeItem('token');
+    setUserAuth({ ...userAuth, id: '', auth: false, refreshToken: '' });
     goHome();
   };
 
   const goHome = () => {
-    navigate("/");
+    navigate('/');
   };
 
   const goLogin = () => {
-    navigate("/login");
+    navigate('/login');
   };
 
   const goBoard = () => {
-    navigate("/board");
+    navigate('/board');
   };
 
   const pathname = href.pathname;
@@ -126,13 +127,13 @@ function App() {
   //     }
   //   });
   // }, [href.pathname]);
-  axios.defaults.headers.common["Authorization"] = `${
-    localStorage.getItem("token") || undefined
+  axios.defaults.headers.common['Authorization'] = `${
+    localStorage.getItem('token') || undefined
   }`;
   useEffect(() => {
-    if (localStorage.getItem("token")) {
+    if (localStorage.getItem('token')) {
       //앞으로 api통신에 토큰이 들어가있음
-      const token = localStorage.getItem("token");
+      const token = localStorage.getItem('token');
 
       const payload = jwtDecode(token);
       setUserAuth({
@@ -143,17 +144,17 @@ function App() {
       });
 
       axios.post(`${URL}/api/middlewere`).then((res) => {
-        if (res.data === "") {
-          console.log("헤더가 없습니다..");
+        if (res.data === '') {
+          console.log('헤더가 없습니다..');
           return;
         }
-        axios.defaults.headers.common["Authorization"] = `${res.data}`;
-        localStorage.setItem("token", res.data);
+        axios.defaults.headers.common['Authorization'] = `${res.data}`;
+        localStorage.setItem('token', res.data);
       });
     }
-    if (localStorage.getItem("token") === null) {
-      console.log("토큰없음");
-      axios.defaults.headers.common["Authorization"] = `${undefined}`;
+    if (localStorage.getItem('token') === null) {
+      console.log('토큰없음');
+      axios.defaults.headers.common['Authorization'] = `${undefined}`;
     }
   }, [href.pathname]);
 
@@ -167,30 +168,30 @@ function App() {
       <UserInfo.Provider value={userAuthContext}>
         <Routes>
           <Route element={<Header />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/drugstore/olive" element={<Oliveyoung />} />
-            <Route path="/clothes/aland" element={<Aland />} />
-            <Route path="/clothes/mustit" element={<Mustit />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/sorry" element={<NotReady />} />
-            <Route path="/mypage" element={<MyPage />} />
-            <Route path="/write" element={<Write />} />
-            <Route path="/board" element={<Board />} />
+            <Route path='/' element={<Home />} />
+            <Route path='/drugstore/olive' element={<Oliveyoung />} />
+            <Route path='/clothes/aland' element={<Aland />} />
+            <Route path='/clothes/mustit' element={<Mustit />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/sorry' element={<NotReady />} />
+            <Route path='/mypage' element={<MyPage />} />
+            <Route path='/write' element={<Write />} />
+            <Route path='/board' element={<Board />} />
             <Route
-              path="/board/viewboard/:boardnumber"
+              path='/board/viewboard/:boardnumber'
               element={<ViewBoard />}
             />
             <Route
-              path="/board/UpdateWrite/:boardnumber"
+              path='/board/UpdateWrite/:boardnumber'
               element={<UpdateWrite data={href.pathname} />}
             />
 
-            <Route path="/comment" element={<NotReady />} />
+            <Route path='/comment' element={<NotReady />} />
           </Route>
-          <Route path="/category" element={<Category />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/auth/passwordchange" element={<PasswordChange />} />
+          <Route path='/category' element={<Category />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/auth/passwordchange' element={<PasswordChange />} />
         </Routes>
         {/* <Footer /> */}
         {viewBoard ? null : <Footerbar />}
