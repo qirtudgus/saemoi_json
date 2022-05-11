dd95a880f239d07874f00fa3a742599ed927d7a8
qirtudgus <qirtudgus@naver.com>
2022-05-11 23:43:13 +0900

    즐겨찾기 서버 테스트

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ window.Buffer = window.Buffer || require('buffer').Buffer;
 const local = 'http://localhost:3001';
 const server = 'https://sungtt.com';
 // URL 할당에 따른 서버환경 변경! 아주편리하다~
-const URL = local;
+const URL = server;
 
 // let vh = window.innerHeight * 0.01;
 // document.documentElement.style.setProperty('--vh', `${vh}px`);
